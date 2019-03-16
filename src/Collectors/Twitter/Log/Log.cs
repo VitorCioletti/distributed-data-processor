bb572b3e955c19dbe1f21fb2068c989938c88c9e
VitorCioletti <ciolettiv@gmail.com>
2019-03-16 10:29:37 -0300

    Added console log writing

--- src/Collectors/Twitter/Log/Log.cs
+++ src/Collectors/Twitter/Log/Log.cs
@@ -2,11 +2,17 @@ namespace Twitter
 {
     using System;
     using LLibrary;
+    using static System.Console;
 
     public static class Log
     {
         private static L _log = new L();
 
-        public static void Write(string label, string log) => _log.Log(label, log);
+        public static void Write(string label, string log)
+        {
+            _log.Log(label, log);
+
+            WriteLine($"{DateTime.Now} {label.ToUpper()} - {log}");
+        }
     }
 }
